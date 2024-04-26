5a53dc5e070ee61ebc4875d58174ae927b35c976
mugeni02 <mugenir7@gmail.com>
2024-04-26 02:49:53 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
-# install flask -v 2.1.0
+# Install Flask version 2.1.0
+package { 'python3-pip':
+  ensure => present,
+}
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => ['/usr/bin'],
+  creates     => '/usr/local/lib/python3.5/dist-packages/flask-2.1.0.dist-info',
+  timeout     => 600,
+  environment => ['HOME' => '/root'],
 }
